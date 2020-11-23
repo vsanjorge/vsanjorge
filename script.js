e0bc6efa5561b9587f8fff376f6763bf1d084a42
Vsanjorge <vsanjorgeperez@gmail.com>
2020-11-23 19:41:28 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,2 @@
+console.log("\u00AF\\_(ツ)_/\u00AF");
+alert("\u0028\u256F\u00B0\u25A1\u00B0\uFF09\u256F\uFE35\u0020\u253B\u2501\u253B\nEste sitio web se encuentra en construcción. Disculpa las molestias.");
