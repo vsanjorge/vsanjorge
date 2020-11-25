c5ea76ea159291353db95e30d0f164bd640e0a7d
Vsanjorge <vsanjorgeperez@gmail.com>
2020-11-25 08:11:13 +0000

    Clean

--- script.js
+++ script.js
@@ -1,2 +1,1 @@
 console.log("From Galicia with 💙");
-//alert("\u0028\u256F\u00B0\u25A1\u00B0\uFF09\u256F\uFE35\u0020\u253B\u2501\u253B\nEste sitio web se encuentra en construcción. Disculpa las molestias.");
